9314402ec01fd0372dbce4fea70ced60433a624b
Nafizul Alam <shohebnafiz@gmail.com>
2023-07-16 22:39:07 +0600

    home page done

---
+++ js/classes.js
@@ -0,0 +1,27 @@
+const classes = [
+    {
+        img: "https://i.ibb.co/6Z8wDd4/yannic-laderach-Dqx4-XWu-Xu7w-unsplash.jpg",
+        descp: "Structure, powerful practice that focuses on mastery of poses and progression to more advanced levels",
+    },
+    {
+        img: "https://i.ibb.co/x8DxV5q/alonso-reyes-0-Hl-I76m4jx-U-unsplash.jpg",
+        descp: "",
+    },
+    {
+        img: "https://i.ibb.co/Y3yLshZ/ young-healthy-man-athlete-doing-exercise-with-ropes-gym-single-male-model-practicing-hard-training-h.jpg",
+        descp: "",
+
+    },
+    {
+        img: "https://i.ibb.co/41bdFt7/young-fitness-man-studio.jpg",
+        descp: "",
+    },
+    {
+        img: "https://i.ibb.co/K7Y19tW/confident-young-muscular-caucasian-athlete-practicing-gym-with-weights-male-model-doing-strength-exe.jpg",
+        descp: "",
+    },
+    {
+        img: "https://i.ibb.co/yVGXZgQ/froehliche-starke-sportlehrerin-die-zeigt-wie-training-crossfit-mit-medizinball-auf-der-schulter-lae.jpg",
+        descp: "",
+    }
+]
